903c5a09ea613442742ec5558aaafd8fef5901bd
imouren <mouren11@163.com>
2017-05-14 01:56:13 +0800

    x

--- cms_mezzanine/settings.py
+++ cms_mezzanine/settings.py
@@ -111,8 +111,8 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-# LANGUAGE_CODE = "en"
-LANGUAGE_CODE = 'zh.CN'
+LANGUAGE_CODE = "en"
+# LANGUAGE_CODE = 'zh.CN'
 
 # Supported languages
 LANGUAGES = (
